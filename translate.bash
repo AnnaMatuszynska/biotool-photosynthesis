4b793da28d43922885ebd477582abbcc61ee6aa4
Marvin van Aalst <marvin.vanaalst@gmail.com>
2023-07-06 12:47:35 +0200

    dev: chg: use loop instead of manual names for translate

--- translate.bash
+++ translate.bash
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-msgfmt -o locales/simple/en/LC_MESSAGES/b-Analyse.mo locales/simple/en/LC_MESSAGES/b-Analyse
-msgfmt -o locales/simple/en/LC_MESSAGES/b-brain.mo locales/simple/en/LC_MESSAGES/b-brain
-msgfmt -o locales/simple/en/LC_MESSAGES/b-messmeth.mo locales/simple/en/LC_MESSAGES/b-messmeth
-msgfmt -o locales/simple/en/LC_MESSAGES/b-model.mo locales/simple/en/LC_MESSAGES/b-model
-msgfmt -o locales/simple/en/LC_MESSAGES/base.mo locales/simple/en/LC_MESSAGES/base
-
-msgfmt -o locales/expert/en/LC_MESSAGES/b-Analyse.mo locales/expert/en/LC_MESSAGES/b-Analyse
-msgfmt -o locales/expert/en/LC_MESSAGES/b-brain.mo locales/expert/en/LC_MESSAGES/b-brain
-msgfmt -o locales/expert/en/LC_MESSAGES/b-messmeth.mo locales/expert/en/LC_MESSAGES/b-messmeth
-msgfmt -o locales/expert/en/LC_MESSAGES/b-model.mo locales/expert/en/LC_MESSAGES/b-model
-msgfmt -o locales/expert/en/LC_MESSAGES/base.mo locales/expert/en/LC_MESSAGES/base
-
-
-msgfmt -o locales/simple/de/LC_MESSAGES/b-Analyse.mo locales/simple/de/LC_MESSAGES/b-Analyse
-msgfmt -o locales/simple/de/LC_MESSAGES/b-brain.mo locales/simple/de/LC_MESSAGES/b-brain
-msgfmt -o locales/simple/de/LC_MESSAGES/b-messmeth.mo locales/simple/de/LC_MESSAGES/b-messmeth
-msgfmt -o locales/simple/de/LC_MESSAGES/b-model.mo locales/simple/de/LC_MESSAGES/b-model
-msgfmt -o locales/simple/de/LC_MESSAGES/base.mo locales/simple/de/LC_MESSAGES/base
-
-msgfmt -o locales/expert/de/LC_MESSAGES/b-Analyse.mo locales/expert/de/LC_MESSAGES/b-Analyse
-msgfmt -o locales/expert/de/LC_MESSAGES/b-brain.mo locales/expert/de/LC_MESSAGES/b-brain
-msgfmt -o locales/expert/de/LC_MESSAGES/b-messmeth.mo locales/expert/de/LC_MESSAGES/b-messmeth
-msgfmt -o locales/expert/de/LC_MESSAGES/b-model.mo locales/expert/de/LC_MESSAGES/b-model
-msgfmt -o locales/expert/de/LC_MESSAGES/base.mo locales/expert/de/LC_MESSAGES/base
+find locales/ -name "*.po" | while read F; do
+    msgfmt -o ${F::-3}.mo ${F::-3}
+done
