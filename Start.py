9434067f1f154949426335ad3e6d8c63a7358f5c
Marvin van Aalst <marvin.vanaalst@gmail.com>
2023-07-06 14:26:31 +0200

    dev: chg: main page

--- Start.py
+++ Start.py
@@ -12,97 +12,17 @@ def make_introduction(text: Callable[[str], str]) -> None:
     st.markdown(text("INTRO"))  # added by Anna
     st.video("https://youtu.be/pqFOIhzmGuk")
     st.markdown(text("HEADLINE_USAGE"))
-    st.markdown(text("MOTIVATION"))
-    st.markdown(text("PROCESS"))
-    st.markdown(text("CONTINUING_TASK"))
-    st.markdown(text("MOTIVATION_2.0"))
-    st.markdown(text("DECLARATION"))
+    st.markdown(text("USAGE"))
+    st.markdown(text("ADVANCED_USE"))
+    st.markdown(text("DROP_BOX_INTRO"))
 
 
 # FIXME: version and language should probably be replaced by text
 def make_chapters(text: Callable[[str], str], version: str, language: str) -> None:
-    with st.expander(text("EXPANDER_CLIMATE")):
-        st.markdown(text("INTRODUCTION_CLIMATE_CHANGE"))
-
-        if version == "expert":
-            st.markdown(text("POINT_1"))
-            st.markdown(text("POINT_2"))
-            st.markdown(text("POINT_3"))
-            st.markdown(text("END_OF_INTRODUCTION"))
-
-        else:
-            col1, col2 = st.columns(2)
-            with col1:
-                if language == "German":
-                    st.image("pictures/pflanzen_grundlagen.jpeg")
-                if language == "English":
-                    st.image("pictures/plants_basics.jpeg")
-            with col2:
-                if language == "German":
-                    st.image("pictures/pflanzen_stress.jpeg")
-                if language == "English":
-                    st.image("pictures/plants_stress.jpeg")
-
-    with st.expander(text("EXPANDER_NUTRIENTS")):
-        st.markdown(text("EXPANDER_NUTRIENTS_EXPLANATION"))
-        image = Image.open("pictures/Stomata1.jpg")
-        st.image(image, caption=text("CAPTION_STOMATA_PICTURE"), width=400)  # make a caption
-
-    with st.expander(text("EXPANDER_PHOTOSYNTHESIS")):
-        st.markdown(text("EXPANDER_PHOTOSYNTHESIS_EXPLANATION_1"))
-        st.markdown(text("EXPANDER_PHOTOSYNTHESIS_EXPLANATION_2"))
-        if language == "German":
-            image = Image.open("pictures/Fotosynthese.jpg")
-            st.image(image, caption=text("CAPTION_FOTOSYNTHESE_PICTURE"))
-        else:
-            image = Image.open("pictures/Fotosynthese_eng.jpg")
-            st.image(image, caption=text("CAPTION_FOTOSYNTHESE_PICTURE"))
-
-    with st.expander(text("EXPANDER_PHOTOSYNTHESIS_LOCATION")):
-        st.markdown(text("EXPANDER_PHOTOSYNTHESIS_LOCATION_EXPLANATION"))
-        if language == "German":
-            image = Image.open("pictures/Fotosynthese-Apparat.jpg")
-            st.image(image, caption=text("CAPTION_FOTOSYNTHESE_APPARAT_PICTURE"), width=600)
-        else:
-            image = Image.open("pictures/Fotosynthese-Apparat_eng.jpg")
-            st.image(image, caption=text("CAPTION_FOTOSYNTHESE_APPARAT_PICTURE"), width=600)
-
-        st.video("https://youtu.be/BU-R724Jyng")
-
-
-    with st.expander(text("EXPANDER_DIFFERENTIAL_EQUATIONS")):
-        st.markdown(text("EXPANDER_DIFFERENTIAL_EQUATIONS_EXPLANATION_1"))
-        st.markdown(text("EXPANDER_DIFFERENTIAL_EQUATIONS_EXPLANATION_2"))
-        st.markdown(text("EXPANDER_DIFFERENTIAL_EQUATIONS_EXPLANATION_3"))
-
-
-# FIXME: version & language should probably be replaced by text
-def make_credits(text: Callable[[str], str], version: str, language: str) -> None:
-    st.markdown(text("CREDITS_ANNA"))
-
-    with st.expander(text("Literature")):
-        st.markdown(text("INTRODUCTION_LITERATURE"))
-        if version == "expert":
-            st.markdown(
-                "- Stirbet, A. et al (2020). Photosynthesis: basic, history and modelling."
-                "Annals of botany vol 126,4: 511-537: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7489092/"
-            )
-            st.markdown("- Holzner, Steven. Differential equations for dummies. John Wiley & Sons, 2008.")
-        elif version == "simple":
-            if language == "German":
-                st.markdown(
-                    "- https://simpleclub.com/lessons/biologie-fotosynthese#:~:text="
-                    "Bei%20der%20Fotosynthese%20erzeugen%20gr%C3%BCne,als%20Energiequelle%20f%C3%BCr%20die%20Pflanze."
-                )
-            else:
-                st.markdown(
-                    "- Fromme, Petra, and Ingo Grotjohann. Overview of photosynthesis. Photosynthetic Protein Complexes: A \
-                    Structural Approach (2008): 1-22."
-                )
-                st.markdown("- Holzner, Steven. Differential equations for dummies. John Wiley & Sons, 2008.")
-                st.markdown(
-                    "- May, Elizabeth, and John Kidder. Climate Change for Dummies. John Wiley & Sons, 2022."
-                )
+    with st.expander(text("EXPANDER_IN")):
+        st.markdown(text("EXPLANATION_IN_VITRO"))
+        st.markdown(text("EXPLANATION_IN_VIVO"))
+        st.markdown(text("EXPLANATION_IN_SILICO"))
 
 
 if __name__ == "__main__":
@@ -157,5 +77,4 @@ if __name__ == "__main__":
     )
     make_introduction(text)
     make_chapters(text, version, language)
-    make_credits(text, version, language)
     make_prev_next_button(None, "photosynthesis")
