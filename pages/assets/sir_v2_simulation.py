ce06be8fb93fe92de475d0c888f529dc750d4730
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2023-07-08 15:45:22 +0200

    dev: new: added plotting

--- pages/assets/sir_v2_simulation.py
+++ pages/assets/sir_v2_simulation.py
@@ -1,4 +1,31 @@
 s = Simulator(sir)
 s.initialise({"S": 900, "I": 100, "R": 0})
 res = s.simulate(t_end=20)
-fig, ax = s.plot(xlabel="Time / months", ylabel="Population size")
+
+# Create the same figure as above
+with plt.rc_context(
+    {
+        "axes.spines.right": False,
+        "axes.spines.top": False,
+        "axes.prop_cycle": cycler(color=["#70A288", "#D5896F", "#8E7DBE"]),
+    }
+):
+    s.plot(xlabel="Months",
+           ylabel="Population size",
+           grid = False,
+           plot_kwargs = {'label': ['S', 'I', 'R']},
+           label_kwargs = {'fontsize': 18},
+           tick_kwargs = {'axis': 'both', 'labelsize': 12},
+           figure_kwargs = {'figsize': (7, 4)}, 
+          )
+
+# Change the left and down limit
+plt.xlim(0)
+plt.ylim(0)
+
+# Create and center legend
+plt.legend(loc = 'center right', frameon = False)
+
+# Show the plot
+plt.show()
+
