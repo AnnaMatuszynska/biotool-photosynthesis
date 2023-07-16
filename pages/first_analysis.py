c489c2c8346edd31232900b2773f787354704234
Elouen Corvest <elouen.corvest@rwth-aachen.de>
2023-07-16 22:44:56 +0200

    dev: chg: Implementation moved

--- pages/first_analysis.py
+++ pages/first_analysis.py
@@ -173,6 +173,65 @@ def expert_plot(NPQ: Any, tm: Any, PhiPSII: Any, areas: Any, chart_labels: Any) 
 
 
 def make_page(text: Callable[[str], str]) -> None:
+    st.markdown(text("HEADLINE_MODEL_CONSTRUCTION"))
+
+    st.markdown(text("CONSTRUCTION_EXPLANATION"))
+    st.markdown(text("RATES_1"), unsafe_allow_html=True)
+    st.markdown(text("RATES_2"))
+    st.markdown(text("RATES_3"))
+    st.markdown(text("RATES_4"))
+    st.markdown(text("RATES_5"))
+    st.markdown(text("RATES_6"), unsafe_allow_html=True)
+
+    if version == "Advanced":
+        st.markdown(text("HEADLINE_MODEL_EQUATIONS"))
+        st.markdown(text("MODEL_EQUATIONS_INTRODUCTION"))
+        st.latex(
+            r"""
+            \begin{aligned}
+                \frac{\mathrm{dPQH_2}}{\mathrm{d}t} &= v_\mathrm{PSII} - v_\mathrm{PQ_{ox}} \\
+                \frac{\mathrm{dATP}}{\mathrm{d}t} &= v_\mathrm{ATPsynthase} - v_\mathrm{ATPconsumption} \\
+                \frac{\mathrm{dATPase^{*}}}{\mathrm{d}t} &= F k_\mathrm{actATPase} \cdot \mathrm{H}(\mathrm{PFD}) \cdot \mathrm{ATPase} - k_\mathrm{deactATPase} \cdot (1 - \mathrm{H}(\mathrm{PFD})) \cdot \mathrm{ATPase^{*}} \\
+                b_\mathrm{H}\cdot\frac{\mathrm{dH}}{\mathrm{d}t} &= 2 v_\mathrm{PSII} + 4 v_\mathrm{PQ_{ox}} -\frac{14}{3} v_\mathrm{ATPsynthase} - v_\mathrm{leak} \\
+                \frac{\mathrm{dPsbS}}{\mathrm{d}t} &= -v_\mathrm{Psbs^{p}} \\
+                \frac{\mathrm{dVx}}{\mathrm{d}t} &= -v_\mathrm{Xcyc} \\
+            \end{aligned}
+        """
+        )
+
+        with st.expander(text("REACTION_RATES")):
+            st.markdown(text("RATES_DYNAMIC"))
+            st.latex(
+                r"""
+                \begin{aligned}
+                    v_{\mathrm{PSII}} &= k_2 \cdot 0.5 \cdot B_1 \\
+                    v_{PQ_{ox}} 7 &= \left(\frac{k_{Cyt_{b6f}} \cdot \mathrm{PFD} \cdot K_\mathrm{eq,cytb6f}(\mathrm{pH})}{K_\mathrm{eq,cytb6f}(\mathrm{pH}) + 1} + k_\mathrm{PTOX}\right) \cdot \mathrm{PQH_2} - \frac{k_\mathrm{PFD}}{K_\mathrm{eq,cytb6f}(\mathrm{pH}) + 1} \cdot \mathrm{PQ} \\
+                    v_\mathrm{ATPsynthase} &= \mathrm{ATPase}^* \cdot k_\mathrm{ATPsynthase}\cdot \left(AP^{tot}-\mathrm{ATP} - \frac{\mathrm{ATP}}{K_\mathrm{eq,ATPsynthase}(H)} \right) \\
+                    v_\mathrm{ATPactivity} &= k_\mathrm{actATPase} \cdot \mathrm{H}(\mathrm{PFD}) \cdot \mathrm{ATPase} - k_\mathrm{deactATPase} \cdot (1 - \mathrm{H}(\mathrm{PFD})) \cdot \mathrm{ATPase^{*}} \\
+                    v_\mathrm{Leak} &= k_\mathrm{leak} \cdot (H - pH_{\mathrm{inv}}(\mathrm{pH_{stroma}})) \\
+                    v_\mathrm{ATPconsumption} &= k_\mathrm{ATPconsumption} \cdot \mathrm{ATP} \\
+                \end{aligned}
+                """
+            )
+
+            st.markdown(text("RATE_QUENCHER"))
+            st.latex(
+                r"""
+                \begin{aligned}
+                    v_\mathrm{Xcyc} &= k_\mathrm{DeepoxV} \cdot \frac{H^\mathrm{nH_X}}{H^\mathrm{nH_X} + pH_{\mathrm{inv}}(K_\mathrm{phSat})^\mathrm{nH_X}} \cdot \mathrm{Vx} - k_\mathrm{EpoxZ} \cdot (\mathrm{X^{tot}} - \mathrm{Vx}) \\
+                    v_\mathrm{Psbs^{p}} &= k_\mathrm{ProtonationL} \cdot \frac{H^\mathrm{nH_L}} {H^\mathrm{nH_L} + pH_{\mathrm{inv}}(K_\mathrm{phSatLHC})^\mathrm{nH_L}} \cdot \mathrm{PsbS} - k_\mathrm{Deprotonation} \cdot \mathrm{PsbS^p} \\
+                    Q &= \gamma_0 \cdot (1-\frac{\mathrm{Zx}}{\mathrm{Zx}+K_\mathrm{{ZSat}}})\cdot\mathrm{PsbS}+ \gamma_1\cdot(1-\frac{\mathrm{Zx}}{\mathrm{Zx}+K_\mathrm{{ZSat}}})\cdot\mathrm{PsbS^p} + \gamma_2\cdot \frac{\mathrm{Zx}}{\mathrm{Zx}+K_\mathrm{{ZSat}}}\cdot\mathrm{PsbS^p} + \gamma_3\cdot \frac{\mathrm{Zx}}{\mathrm{Zx}+K_\mathrm{{ZSat}}}\cdot\mathrm{PsbS} \\
+                \end{aligned}
+                """
+            )
+
+    st.markdown(text("HEADLINE_IMPLEMENTATION"))
+
+    st.markdown(text("IMPLEMENTATION_DESCRIPTION"))
+
+    if version == "Simple":
+        st.markdown(text("IMPLEMENTATION_TO_EXPERT"))
+
     # UI (Mainpage-Website)
     st.markdown(text("HEADLINE_ANALYSE"))
     st.markdown(text("INTRODUKTION"))
