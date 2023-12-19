b65979b6417e4fcc28bedccae766f925e5af5787
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2023-12-19 12:47:59 +0100

    dev: new: Made Contact Page translatable

--- pages/contact.py
+++ pages/contact.py
@@ -14,61 +14,56 @@ def make_page(text: Callable[[str], str], language: str, version: str):
         st.toast(":blue[Thank you for finishing our Biotool!]", icon="🎉")
         st.session_state["ballooned"] = True
 
-    st.header(":mailbox: Contact Us!")
+    st.header(":mailbox: " + text("CONT_HEADER"))
 
-    st.markdown(
-        "We welcome your feedback and questions about our project! Here are the best ways to get in touch with our team members:"
-    )
-
+    st.markdown(text("CONT_SUBHEADER"))
+    
     st.markdown(
         "### Sarah Philipps\n"
-        "- **Email:** sarah.philipps@rwth-aachen.de\n"
-        "- **Topics:** education, motivation behind the platform, and general inquiries\n"
+        f"- **{text('CONT_EMAIL')}** sarah.philipps@rwth-aachen.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_SARAH')}\n"
     )
 
     st.markdown(
         "### Tobias Pfennig\n"
-        "- **Email:** tobias.pfennig@rwth-aachen.de\n"
-        "- **Topics:** fluorescence measuring methods and modelling of photosynthesis\n"
-        "- **Website:** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~wljpm/Tobias-Pfennig/)\n"
+        f"- **{text('CONT_EMAIL')}** tobias.pfennig@rwth-aachen.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_TOBIAS')}\n"
+        f"- **{text('CONT_WEBSITE')}** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~wljpm/Tobias-Pfennig/)\n"
     )
 
     st.markdown(
-        "### Elouën Corvest\n"
-        "- **Email:** elouen.corvest@rwth-aachen.de\n"
-        "- **Topics:** available computational models of photosynthesis"
-        "- **Website:** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~wljuk/Elouen-Corvest/lidx/1/)\n"
+        "### Elouen Corvest\n"
+        f"- **{text('CONT_EMAIL')}** elouen.corvest@rwth-aachen.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_ELOUEN')}\n"
+        f"- **{text('CONT_WEBSITE')}** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~wljuk/Elouen-Corvest/lidx/1/)\n"
     )
 
     st.markdown(
         "### Marvin van Aalst\n"
-        "- **Email:** marvin.van.aalst@hhu.de\n"
-        "- **Topics:** technical questions about the platform, programming and software development\n"
-        "- **Website:** [Heinrich Heine University Duesseldorf](https://www.qtb.hhu.de/qtb-team/qtb-team-details?tt_address%5Bfunktion%5D=26702&tt_address%5Bperson%5D=21874&cHash=9ebb911fc0f89ce42ec1fd253420bca6)\n"
+        f"- **{text('CONT_EMAIL')}** marvin.van.aalst@hhu.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_MARVIN')}\n"
+        f"- **{text('CONT_WEBSITE')}** [Heinrich Heine University Duesseldorf](https://www.qtb.hhu.de/qtb-team/qtb-team-details?tt_address%5Bfunktion%5D=26702&tt_address%5Bperson%5D=21874&cHash=9ebb911fc0f89ce42ec1fd253420bca6)\n"
     )
 
     st.markdown(
         "### Lisa Fürtauer\n"
-        "- **Email:** lisa.fuertauer@bio3.rwth-aachen.de\n"
-        "- **Topics:** research on photosynthesis in the context of climat change\n"
-        "- **Website:** [RWTH Aachen](https://www.bio3.rwth-aachen.de/cms/BIO3/Das-Institut/~tcejj/Juniorprofessur-Molekulare-Systembiologi/)\n"
+        f"- **{text('CONT_EMAIL')}** lisa.fuertauer@bio3.rwth-aachen.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_LISA')}\n"
+        f"- **{text('CONT_WEBSITE')}** [RWTH Aachen](https://www.bio3.rwth-aachen.de/cms/BIO3/Das-Institut/~tcejj/Juniorprofessur-Molekulare-Systembiologi/)\n"
     )
 
     st.markdown(
         "### Anna Matuszyńska\n"
-        "- **Email:** anna.matuszynska@rwth-aachen.de\n"
-        "- **Topics:** general project inquiries, computational modelling, photosynthesis\n"
-        "- **Website:** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~ywkwc/Anna-Matuszy-324-ska/)\n"
+        f"- **{text('CONT_EMAIL')}** anna.matuszynska@rwth-aachen.de\n"
+        f"- **{text('CONT_TOPICS')}** {text('CONT_TOPICS_ANNA')}\n"
+        f"- **{text('CONT_WEBSITE')}** [RWTH Aachen](https://www.cpbl.rwth-aachen.de/cms/CPBL/Die-Juniorprofessur/Unser/~ywkwc/Anna-Matuszy-324-ska/)\n"
     )
 
-    st.markdown(
-        "Please note that our team members may have different response times due to their schedules and \
-                    responsibilities. We will do our best to respond to your messages as soon as possible."
-    )
+    st.markdown(text("CONT_HOURS"))
 
-    st.markdown("Thank you for your interest in our project!")
+    st.markdown(text("CONT_THANKS"))
 
-    st.markdown("### Resources")
+    st.markdown(text("CONT_RESOURCES"))
     st.markdown(text("PROGRAMS_USED"))
 
 
